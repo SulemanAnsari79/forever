800b034ef1e34e3670e18f8b447a2120f60ed349
Mohammad Suleman <official.suleman01@gmailcom>
2025-07-23 20:27:54 +0530

    Best seller section added

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState,useEffect } from 'react'
 import { ShopContext } from '../context/shopContext'
+import ProductItem from './ProductItem';
 import Title from './Title';
 
 const BestSeller = () => {
@@ -10,9 +11,9 @@ const BestSeller = () => {
 
 
     useEffect(() => {
-        const bestProduct=products.filter((items)=>items.bestSeller)
+        const bestProduct = products.filter(item => item.bestseller);
         setBestSeller(bestProduct.slice(0,5));
-    }, [products])
+    }, [])
     
 
 
@@ -26,7 +27,7 @@ const BestSeller = () => {
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {
                     bestSeller.map((item,index)=>(
-                            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
                     ))
                 }
         </div>
